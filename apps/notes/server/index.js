8a5a76e5dff9dd79eeed50edada148aebb29b2cb
IvanInandan <c.ivaninandan@gmail.com>
2024-11-01 19:26:56 -0700

    Fix styling errors with eslint

--- apps/notes/server/index.js
+++ apps/notes/server/index.js
@@ -9,97 +9,89 @@ app.use(express.json())
 app.use(cors())
 app.use(morgan('tiny'))
 
-/*const generateId = () => {
-    const maxId = notes.length > 0
-    ? Math.max(...notes.map(n => Number(n.id)))
-    : 0
-
-    return String(maxId + 1)
-}*/
-
 const errorHandler = (error, request, response, next) => {
-    console.error(error.message)
+  console.error(error.message)
 
-    if (error.name === 'CastError') {
-        return response.status(400).send({ error: 'malformatted id' })
-    }
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
 
-    next(error)
+  next(error)
 }
 
 app.use(errorHandler)
 
 app.get('/', (request, response) => {
-    response.send('<h1>Hello World!</h1>')
+  response.send('<h1>Hello World!</h1>')
 })
 
 app.get('/api/notes', (request, response) => {
-    Note.find({}).then(notes => {
-        response.json(notes)
-    })
+  Note.find({}).then(notes => {
+    response.json(notes)
+  })
 })
 
 app.get('/api/notes/:id', (request, response, next) => {
-    Note
-        .findById(request.params.id)
-        .then(note => {
-            if (note) {
-                response.json(note)
-            } else {
-                response.status(404).end()
-            }
-        })
-        .catch(error => next(error))
+  Note
+    .findById(request.params.id)
+    .then(note => {
+      if (note) {
+        response.json(note)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => next(error))
 })
 
 app.delete('/api/notes/:id', (request, response, next) => {
-    Note
-        .findByIdAndDelete(request.params.id)
-        .then(result => {
-            if (result) {
-                response.status(204).end()
-            } else {
-                response.status(404).send({ error: 'id not found' })
-            }
-        })
-        .catch(error => next(error))
+  Note
+    .findByIdAndDelete(request.params.id)
+    .then(result => {
+      if (result) {
+        response.status(204).end()
+      } else {
+        response.status(404).send({ error: 'id not found' })
+      }
+    })
+    .catch(error => next(error))
 })
 
 app.post('/api/notes', (request, response) => {
-    const body = request.body
+  const body = request.body
 
-    if (!body.content) {
-        return response.status(400).json({
-            error: 'Content missing'
-        })
-    }
-
-    const note = new Note({
-        content: body.content,
-        important: body.important || false,
+  if (!body.content) {
+    return response.status(400).json({
+      error: 'Content missing'
     })
+  }
 
-    note.save().then(savedNote => {
-        response.json(savedNote)
-    })
+  const note = new Note({
+    content: body.content,
+    important: body.important || false,
+  })
+
+  note.save().then(savedNote => {
+    response.json(savedNote)
+  })
 })
 
 app.put('/api/notes/:id', (request, response, next) => {
-    const { content, important } = request.body
+  const { content, important } = request.body
 
-    Note
-        .findByIdAndUpdate(
-            request.params.id,
-            { content, important },
-            { new: true, runValidators: true, context:'query' }
-        )
-        .then(updatedNote => {
-            response.json(updatedNote)
-        })
-        .catch(error => next(error))
+  Note
+    .findByIdAndUpdate(
+      request.params.id,
+      { content, important },
+      { new: true, runValidators: true, context: 'query' }
+    )
+    .then(updatedNote => {
+      response.json(updatedNote)
+    })
+    .catch(error => next(error))
 })
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+  console.log(`Server running on port ${PORT}`)
 })
