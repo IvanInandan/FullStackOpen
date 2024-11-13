75fce153dbb4095209d4fe63a2a7e9a71590d3e8
IvanInandan <c.ivaninandan@gmail.com>
2024-11-13 11:16:07 -0800

    Add beforeEach function for database API testing

--- apps/notes/server/tests/note_api.test.js
+++ apps/notes/server/tests/note_api.test.js
@@ -1,10 +1,31 @@
-const { test, after } = require("node:test");
+const Note = require("../models/note");
+const { test, after, beforeEach } = require("node:test");
 const assert = require("node:assert");
 const mongoose = require("mongoose");
 const supertest = require("supertest");
 const app = require("../app");
 
 const api = supertest(app);
+
+const initialNotes = [
+  {
+    content: "HTML is easy",
+    important: false,
+  },
+  {
+    content: "Browser can only execute JavaScript",
+    important: true,
+  },
+];
+
+// beforeEach function runs before each and every test suite. Used to initialize database
+beforeEach(async () => {
+  await Note.deleteMany({});
+  let noteObject = new Note(initialNotes[0]);
+  await noteObject.save();
+  noteObject = new Note(initialNotes[1]);
+  await noteObject.save();
+});
 
 test("notes are returned as JSON", async () => {
   await api
@@ -15,13 +36,30 @@ test("notes are returned as JSON", async () => {
 
 test("there are two notes", async () => {
   const response = await api.get("/api/notes");
-  assert.strictEqual(response.body.length, 2);
+  assert.strictEqual(response.body.length, initialNotes.length);
 });
 
 test("the first note is about HTTP methods", async () => {
   const response = await api.get("/api/notes");
   const contents = response.body.map((e) => e.content);
-  assert.strictEqual(contents.includes("HTML is x"), true);
+  assert(contents.includes("HTML is easy"));
+});
+
+test("a valid note can be added", async () => {
+  const newNote = {
+    content: "async/await simplifies making async calls",
+    important: true,
+  };
+
+  await api
+    .post("/api/notes")
+    .send(newNote)
+    .expect(201)
+    .expect("Content Type", /application\/json/);
+
+  const response = await api.get("/api/notes");
+  const contents = response.body.map((res) => res.content);
+  assert.strictEqual(response.body.length);
 });
 
 after(async () => {
--- apps/notes/server/utils/logger.js
+++ apps/notes/server/utils/logger.js
@@ -1,9 +1,11 @@
+require("dotenv").config();
+
 const info = (...params) => {
-  console.log(...params);
+  if (process.env.NODE_ENV === "development") console.log(...params);
 };
 
 const error = (...params) => {
-  console.error(...params);
+  if (process.env.NODE_ENV === "deveopment") console.error(...params);
 };
 
 module.exports = { info, error };
