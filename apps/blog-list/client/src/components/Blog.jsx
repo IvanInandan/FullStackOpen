fbfd31ec33eb20f28663b8881e1f1c026278a4b7
IvanInandan <c.ivaninandan@gmail.com>
2025-02-03 11:03:09 -0800

    Add button to toggle allInfo per Blog element

--- apps/blog-list/client/src/components/Blog.jsx
+++ apps/blog-list/client/src/components/Blog.jsx
@@ -1,7 +1,36 @@
-const Blog = ({ blog }) => (
-  <div>
-    Title: {blog.title} - {blog.author}
-  </div>
-);
+import { useState } from "react";
+
+const Blog = ({ blog }) => {
+  const [allInfo, setAllInfo] = useState(false);
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: "solid",
+    borderWidth: 1,
+    marginBottom: 5,
+  };
+
+  console.log(blog);
+
+  return (
+    <div style={blogStyle}>
+      <div>
+        {blog.title} {blog.author}
+        <button onClick={() => setAllInfo(!allInfo)}>
+          {allInfo ? "hide" : "view"}
+        </button>
+      </div>
+
+      {allInfo && (
+        <div>
+          <div>{blog.url}</div>
+          <div>{blog.likes}</div>
+          <div>{blog.id}</div>
+        </div>
+      )}
+    </div>
+  );
+};
 
 export default Blog;
