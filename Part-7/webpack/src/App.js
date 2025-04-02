e846ee216650274df10b6869efd7d21c47d222d5
IvanInandan <c.ivaninandan@gmail.com>
2025-04-02 12:44:16 -0700

    Implement json-server for back-end

--- Part-7/webpack/src/App.js
+++ Part-7/webpack/src/App.js
@@ -1,9 +1,24 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import "./index.css";
+
+const useNotes = (url) => {
+  const [notes, setNotes] = useState([]);
+
+  useEffect(() => {
+    axios.get(url).then((response) => {
+      setNotes(response.data);
+    });
+  }, [url]);
+
+  return notes;
+};
 
 const App = () => {
   const [counter, setCounter] = useState(0);
   const [values, setValues] = useState([]);
+  const url = "https://localhost:3001/notes";
+  const notes = useNotes(url);
 
   const handleClick = () => {
     setCounter(counter + 1);
@@ -14,6 +29,9 @@ const App = () => {
     <div className="container">
       hello webpack {counter} clicks
       <button onClick={handleClick}>press</button>
+      <div>
+        {notes.length} notes on server {url}
+      </div>
     </div>
   );
 };
